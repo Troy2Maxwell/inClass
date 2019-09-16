9d10ee7641d0fa3bd19703717342244937ece1da
Troy Maxwell <54861255+Troy2Maxwell@users.noreply.github.com>
2019-09-16 13:57:26 -0500

    uploading Product Example of Classes

---
+++ Products/Products/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var product = new Product("Nintendo Switch", 299.99m);
+            // product.Name = "Nintendo Switch";
+            // product.Price = 299.99m;
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+            Console.ReadLine();
+        }
+    }
+}
