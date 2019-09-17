76e03ad05fe7322c47a55bee1e11828d6bd28eb1
Troy Maxwell <54861255+Troy2Maxwell@users.noreply.github.com>
2019-09-17 13:26:44 -0500

    Add files via upload

---
+++ TrueCoders/TrueCoders/People.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrueCoders
+{
+    class People
+    {
+        public int Age { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Haircolor { get; set; }
+
+        public People(string firstName, string lastName, int age, string hairColor)
+        {
+            this.Age = age;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Haircolor = hairColor;
+
+            Console.WriteLine($"Name: {firstName} {lastName}, Age: {age}, Hair Color: {hairColor}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ TrueCoders/TrueCoders/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrueCoders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            People person1 = new People("Michael", "Doyle", 32, "Brown");
+            People person2 = new People("Troy", "Maxwell", 30, "Blonde");
+            People person3 = new People("Rod", "Geddie", 30, "Brown");
+
+            List<People> trueCoders = new List<People>();
+            trueCoders.Add(person1);
+            trueCoders.Add(person2);
+            trueCoders.Add(person3);
+        }
+    }
+}
